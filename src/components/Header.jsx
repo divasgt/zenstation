a6e84751e3b05bec36397c06a2ac273fde6407e5
Divas <92243128+divasgt@users.noreply.github.com>
2025-08-23 02:11:35 +0530

    Added fullscreen button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react"
+
 export default function Header({theme, setTheme, setIsCustomTheme}) {
+  const [isFullScreen, setIsFullScreen] = useState(false)
+
+  // useEffect(() => {
+    
+  // })
+
+  function toggleFullScreen() {
+    if (isFullScreen) {
+      document.documentElement.requestFullscreen()
+    } else {
+      document.exitFullscreen()  
+    }
+    setIsFullScreen(prev => !prev)
+  }
+  
   return (
   <header>
     <nav>
@@ -12,6 +29,14 @@ export default function Header({theme, setTheme, setIsCustomTheme}) {
         {/* on clicking above, pause video if playing and show entering link popup */}
       </ul>
     </nav>
+
+    <div className="header-right-div">
+      <button onClick={toggleFullScreen} className="fullscreen-btn">
+        {/* <img src="https://img.icons8.com/?size=100&id=98066&format=png&color=ffffff" alt="fullscreen-icon" style={{width: "20px", marginTop: "2px"}} /> */}
+        {/* <img src="https://img.icons8.com/?size=100&id=hafLDNJlLQnR&format=png&color=ffffff" alt="" style={{width: "22px", marginTop: "2px"}} /> */}
+        <img src="https://img.icons8.com/?size=100&id=ljlb4da7psfA&format=png&color=ffffff" alt="fullscreen-icon" style={{width: "15px", marginTop: "2px"}} />
+      </button>
+    </div>
   </header>
   )
 }
