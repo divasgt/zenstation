a29dd8e6b20851dd3081fc188adf044f475b3cd7
Divas <92243128+divasgt@users.noreply.github.com>
2025-08-23 15:27:46 +0530

    Changed header a elements to buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,6 @@ import { useEffect, useState } from "react"
 export default function Header({theme, setTheme, setIsCustomTheme}) {
   const [isFullScreen, setIsFullScreen] = useState(false)
 
-  // useEffect(() => {
-    
-  // })
-
   function toggleFullScreen() {
     if (isFullScreen) {
       document.documentElement.requestFullscreen()
@@ -20,13 +16,14 @@ export default function Header({theme, setTheme, setIsCustomTheme}) {
   <header>
     <nav>
       <ul>
-        <li><a className="home-nav-link" onClick={() => setTheme("default")}>😎</a></li>
-        <li><a onClick={() => setTheme("lofi")}>Lofi</a></li>
-        <li><a onClick={() => setTheme("cafe")}>Cafe</a></li>
-        <li><a onClick={() => setTheme("library")}>Library</a></li>
-        <li><a onClick={() => setTheme("relax")}>Relax</a></li>
-        <li><a onClick={() => setIsCustomTheme(prev => !prev)}>Create my vibe</a></li>
-        {/* on clicking above, pause video if playing and show entering link popup */}
+        <li><button className="home-nav-link" onClick={() => setTheme("default")}>😎</button></li>
+        <li><button onClick={() => setTheme("lofi")}>Lofi</button></li>
+        <li><button onClick={() => setTheme("cafe")}>Cafe</button></li>
+        <li><button onClick={() => setTheme("library")}>Library</button></li>
+        <li><button onClick={() => setTheme("relax")}>Relax</button></li>
+
+        {/* on clicking below button, pause video if playing and show entering link popup */}
+        <li><button onClick={() => setIsCustomTheme(prev => !prev)}>Create my vibe</button></li>
       </ul>
     </nav>
 
@@ -34,7 +31,7 @@ export default function Header({theme, setTheme, setIsCustomTheme}) {
       <button onClick={toggleFullScreen} className="fullscreen-btn">
         {/* <img src="https://img.icons8.com/?size=100&id=98066&format=png&color=ffffff" alt="fullscreen-icon" style={{width: "20px", marginTop: "2px"}} /> */}
         {/* <img src="https://img.icons8.com/?size=100&id=hafLDNJlLQnR&format=png&color=ffffff" alt="" style={{width: "22px", marginTop: "2px"}} /> */}
-        <img src="https://img.icons8.com/?size=100&id=ljlb4da7psfA&format=png&color=ffffff" alt="fullscreen-icon" style={{width: "15px", marginTop: "2px"}} />
+        <img className="fullscreen-img" src="https://img.icons8.com/?size=100&id=ljlb4da7psfA&format=png&color=ffffff" alt="fullscreen-icon" />
       </button>
     </div>
   </header>
